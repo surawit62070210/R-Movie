f90b95e353450a1acd1f3d6a30d1d5296ac4ea1c
Sakon Teerasuwonajak <flukgoza@gmail.com>
2022-04-26 16:42:12 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const http = require("http");
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 
 let app = express();
@@ -13,20 +14,23 @@ app.use(morgan('dev')); // ให้ app(express) ใช้งานการ mo
 app.use(cors()); // ให้ app(express) ใช้งานการ cors module
 
 const userRouter = require('./routes/user-routes');
+const authRouter = require('./routes/auth-routes')
+app.use(cookieParser());
 app.use('/users', userRouter)
+app.use('/auth', authRouter)
 const port = parseInt(process.env.PORT, 10) || 3000
 
 const client = require('./models/DbConfig')
-const query ='INSERT INTO users (user_name, user_email, user_password, user_firstname, user_lastname, user_nickname) VALUES ($1,$2,$3,$4,$5,$6)'
+// const query ='INSERT INTO users (user_name, user_email, user_password, user_firstname, user_lastname, user_nickname) VALUES ($1,$2,$3,$4,$5,$6)'
 
-client.query(query,["user_name", "user_email", "user_password", "user_firstname", "user_lastname", "user_nickname"], (err, res) => {
-  if (err) {
-      console.error(err);
-      return;
-  }
-  console.log('Databaase Connecting');
-  client.end();
-});
+// client.query(query,["user_name", "user_email", "user_password", "user_firstname", "user_lastname", "user_nickname"], (err, res) => {
+//   if (err) {
+//       console.error(err);
+//       return;
+//   }
+//   console.log('Databaase Connecting');
+//   client.end();
+// });
 // client.query(query, ["user_name", "user_email", "user_password", "user_firstname", "user_lastname", "user_nickname"])
 app.set("port", port)
 server.listen(port, function() {
--- backend/utils/jwt-helpers.js
+++ backend/utils/jwt-helpers.js
@@ -1,11 +1,11 @@
-import jwt from 'jsonwebtoken';
+const jwt = require('jsonwebtoken');
 
 //Generate an access token and a refresh token for this database user
-function jwtTokens({user_name, user_email, user_password, user_firstname, user_lastname, user_nickname}) {
-  const user = { user_id, user_name, user_email,user_password, user_firstname, user_lastname, user_nickname}; 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
-  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '5m' });
+function jwtTokens({user_email, user_password}) {
+  const user = {user_email,user_password}; 
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' });
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '30m' });
   return ({ accessToken, refreshToken });
 }
 
-export {jwtTokens};
+module.exports = {jwtTokens};
