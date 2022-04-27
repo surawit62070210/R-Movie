ac5ed12903ccd36258923c359073c2bdb01a48fb
Nontapat Kupreecha <56498257+nonava172352@users.noreply.github.com>
2022-04-27 20:37:04 +0700

    Update

--- rmovie/src/main.js
+++ rmovie/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
@@ -12,3 +13,4 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app')
+
--- rmovie/src/views/Main-Page.vue
+++ rmovie/src/views/Main-Page.vue
@@ -1,11 +1,25 @@
-<template >
-<div class="card" style="width: 18rem;" v-for="movie in movies" :key="movie">
-  <!-- <img class="card-img-top" src="..." alt="Card image cap"> -->
-  <div class="card-body">
-    <h5 class="card-title">{{movie.title}}</h5>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+<template>
+<body>
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-lg-3 col-md-3 col-sm-3 col-xs-12" v-for="(movie, index) in movies"
+        :key="index" 
+      >
+        <div class="card" style="width: 15rem">
+          <img class="card-img-top" :src="movie.image1" alt="Card image cap" style="max-height: 300px" />
+          <div class="card-body">
+            <h1 class="card-title" style="font-size: 17px; font-weight: 600">{{movie.title}}</h1>
+            <a href="/random" class="btn btn-warning" style="color: white">Watch</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
   </div>
-</div>
+  </body>
 </template>
 
 <script>
@@ -14,20 +28,24 @@ export default {
   data() {
     return {
       movies: [],
-    }
+    };
   },
   async created() {
     try {
-      const res = await axios.get(`http://localhost:3000/`)
+      const res = await axios.get(`http://localhost:3000/`);
 
       this.movies = res.data;
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-    console.log(this.movies[0].rank)
-  }
-  
-}
-
+    console.log(this.movies[0].rank);
+  },
+};
 </script>
 
+<style scoped>
+body {
+  width: 100%;
+  background-color: rgb(0, 0, 0);
+}
+</style>
